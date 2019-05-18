05265580ce1e24c713c0bdcee12fb546f4fcd6f7
Kevin <kevinjdamore@gmail.com>
2019-05-18 14:59:39 -0400

    compilation error fixes

--- Sources/App/Controllers/BattleActionController.swift
+++ Sources/App/Controllers/BattleActionController.swift
@@ -21,19 +21,22 @@ final class BattleActionController: BaseController {
     func createAction(_ req: Request, data: CreateBattleAction) throws -> Future<DataResponse<BattleAction>> {
         guard let _ = try authenticatedFirebaseUser(req) else { throw Abort(.unauthorized) }
         
-        return req.dispatch { request in
-            var battle = try Battle.find(data.battleId, on: request).unwrap(or: Abort(.badRequest, reason: "Battle with id: \(data.battleId) doesn't exist")).wait()
-            guard try BattleTurn.find(data.turnId, on: request).wait() != nil else { throw Abort(.badRequest, reason: "Turn with id: \(data.turnId) doesn't exist") }
-            battle.updateTime = Date().timeIntervalSince1970
-            _ = battle.update(on: request)
-            let newAction = BattleAction.new(from: data)
-            _ = newAction.save(on: request)
-            
-            if newAction.actionType == 3 {
-                print("send notification telling the other user it is their turn!")
-            }
-            
-            return DataResponse<BattleAction>(data: newAction)
-        }
+        throw Abort(.internalServerError)
+//        Battle.find(data.battleId, on: req).unwrap(or: Abort(.badRequest, reason: "Battle with id: \(data.battleId) doesn't exist")
+        
+//        return req.dispatch { request in
+//            let battle = try Battle.find(data.battleId, on: request).unwrap(or: Abort(.badRequest, reason: "Battle with id: \(data.battleId) doesn't exist")).wait()
+//            guard try BattleTurn.find(data.turnId, on: request).wait() != nil else { throw Abort(.badRequest, reason: "Turn with id: \(data.turnId) doesn't exist") }
+//            battle.updateTime = Date().timeIntervalSince1970
+//            _ = battle.update(on: request)
+//            let newAction = BattleAction.new(from: data)
+//            _ = newAction.save(on: request)
+//            
+//            if newAction.actionType == 3 {
+//                print("send notification telling the other user it is their turn!")
+//            }
+//            
+//            return DataResponse<BattleAction>(data: newAction)
+//        }
     }
 }
--- Sources/App/Controllers/BattleTurnController.swift
+++ Sources/App/Controllers/BattleTurnController.swift
@@ -21,15 +21,24 @@ final class BattleTurnController: BaseController {
     func createTurn(_ req: Request, data: CreateBattleTurn) throws -> Future<DataResponse<BattleTurn>> {
         guard let _ = try authenticatedFirebaseUser(req) else { throw Abort(.unauthorized) }
         
-        return req.dispatch { request in
-            var battle = try Battle.find(data.battleId, on: request).unwrap(or: Abort(.badRequest, reason: "Battle with id: \(data.battleId) doesn't exist")).wait()
+        return Battle.find(data.battleId, on: req).unwrap(or: Abort(.badRequest, reason: "Battle with id: \(data.battleId) doesn't exist")).map { battle in
             guard data.userId == battle.attackerId || data.userId == battle.defenderId else { throw Abort(.badRequest, reason: "No user with id: \(data.userId) exists in this battle") }
-            // TODO: make sure heroId exists in db
             battle.updateTime = Date().timeIntervalSince1970
-            _ = battle.update(on: request)
+            _ = battle.update(on: req)
             let newTurn = BattleTurn.new(from: data)
-            _ = newTurn.save(on: request)
+            _ = newTurn.save(on: req)
             return DataResponse<BattleTurn>(data: newTurn)
         }
+    
+//        return req.dispatch { request in
+//            let battle = try Battle.find(data.battleId, on: request).unwrap(or: Abort(.badRequest, reason: "Battle with id: \(data.battleId) doesn't exist")).wait()
+//            guard data.userId == battle.attackerId || data.userId == battle.defenderId else { throw Abort(.badRequest, reason: "No user with id: \(data.userId) exists in this battle") }
+//            // TODO: make sure heroId exists in db
+//            battle.updateTime = Date().timeIntervalSince1970
+//            _ = battle.update(on: request)
+//            let newTurn = BattleTurn.new(from: data)
+//            _ = newTurn.save(on: request)
+//            return DataResponse<BattleTurn>(data: newTurn)
+//        }
     }
 }
